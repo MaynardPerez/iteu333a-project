682c61b5348f98ae13183eed4fc19435236d7675
Jason Galotera <galoterajason@gmail.com>
2016-10-06 08:54:14 +0800

    Adds first regex

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -16,6 +16,7 @@ public class JavaApplication5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("asd");
     }
     
 }
